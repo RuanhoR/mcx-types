f45353a9be3b73ee1ceccb324c67ffbd0783b902
Ruanhor <3915264929@qq.com>
2026-02-13 17:32:22 +0800

    update

---
+++ index.d.ts
@@ -0,0 +1,51 @@
+import type { ParserOptions } from "@babel/parser";
+import type { RollupOptions } from "rollup";
+import type { World } from "@minecraft/server";
+interface CompileUserConfig {
+  babelParser?: ParserOptions;
+  rollupOptions?: RollupOptions;
+}
+interface CompileOpt {
+  main: string;
+  ProjectDir: string;
+  moduleDir: string;
+  output: string;
+  config?: Partial<CompileUserConfig>;
+}
+interface EventOpt {
+  on: "after" | "before";
+  data: Record<string, (event: any) => void>;
+}
+declare class Event {
+  #currenyRun: string[];
+  #canRun: string[];
+  #eventList: EventOpt["data"];
+  #on: "after" | "before";
+  /**
+   *
+   * @param eventName {string} - eventName
+   * @returns {boolean} - if succeess
+   */
+  #bind_event(eventItem: string): boolean;
+  subscribe(...events: string[]): boolean;
+  #unbind_event(eventName: string): boolean;
+  /**
+   * unscribe
+   */
+  unscribe(...events: string[]): boolean;
+  useWorld(_world: World): void;
+}
+type MCXFileType = "app" | "component" | "event";
+interface MCXFileBase extends Object {
+  type: MCXFileType;
+  setup: Record<string, any>;
+}
+interface MCXFile<T extends MCXFileType> extends MCXFileBase {
+  app: T extends "app"
+    ? {
+        event ?: Event;
+      }
+    : never;
+  event: T extends "event" ? Event : never;
+}
+export type { CompileOpt, CompileUserConfig, MCXFile };
